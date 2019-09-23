d555330162add04a81b1073f155a375002382390
Voland <maloyvladis@gmail.com>
2019-09-23 14:33:53 +0300

    First lesson_3

---
+++ KolNum/KolNum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace KolNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int x = int.Parse(Console.ReadLine());
+            int kol = 0;
+            int sum = 0;
+            int KolOrder = 0;
+            while (x != 0)
+            {
+                 if (x % 10 % 2 == 0) 
+                 {
+                  sum = sum + (x % 10);
+                  kol++;
+                         
+                 }
+                    if (x % 10 % 3 == 0)
+                    {
+                    KolOrder++;
+                    }
+                  
+             x = x / 10;
+            }
+         // Console.WriteLine("Сумма четных  " + sum);
+           Console.WriteLine("Кол-во Четных цифр= " + kol + "  Сумма четных цифр = " + sum);
+           Console.WriteLine("Кол-во Чисел кратных 3 :  " + KolOrder++);
+           Console.ReadKey();
+        }
+    }
+}
---
+++ NumSecret/NumSecret/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumSecret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Num = new Random();
+            int RandomNum = Num.Next(0, 100); 
+            int UserNum; 
+            int attempt = 1;     
+                     
+                     
+                Console.WriteLine("I came up with a number from 0 to 100, guess it:");
+                UserNum = Convert.ToInt32(Console.ReadLine());
+
+                while (UserNum != RandomNum)
+                {
+                    Console.Write($"Attempt number № { attempt} = ");
+                    attempt++;
+                    if (attempt == 6) break;
+
+                    Console.WriteLine( (RandomNum > UserNum) ? "My number is bigger " : "My number is less  ");
+                    UserNum = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine((RandomNum == UserNum) ? "Congratulations! You are a winner" : "Sorry! You are a loser", attempt);
+
+                     
+        }
+    }
+}
